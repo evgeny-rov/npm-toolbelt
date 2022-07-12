a45b0aebaad7b5c020eec7655f03ce2874170444
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-07-12 12:22:05 +0800

    added octokit, testing query

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
-}
+import { graphql } from '@octokit/graphql';
+import type { User } from '@octokit/graphql-schema';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `Bearer ${process.env.GH_TOKEN}`,
+  },
+});
+
+const createPackagesQuery = () =>
+  `query packages($username: String!) {
+    user(login: $username) {
+      repositories(
+        isFork: false,
+        first: 100,
+        orderBy: {field: UPDATED_AT, direction: DESC}
+      ) {
+        edges {
+          node {
+            object(expression: "HEAD:package.json") {
+              ... on Blob {
+                text
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+const getUserReposWithPackages = (username: string) => {
+  const query = createPackagesQuery();
+  const response = graphqlWithAuth<{ user: User }>(query, { username });
+
+  return response;
+};
+
+type Data = string[];
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const anyGhRequest = await getUserReposWithPackages('gawg213dsg4y3y4ysfs35@#');
+
+    res.status(200).json(anyGhRequest);
+  } catch (req) {
+    console.log(e.errors);
+    res.status(500).json(e.errors[0].message);
+  }
 }
